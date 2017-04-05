2d41ff69091ff095b17fcbbf2a4d1df196e8b22c
Joe Salter <jcsalter@princeton.edu>
2017-04-05 11:15:27 -0400

    API skeleton for accounts and projects

---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express')
+ 
+var app = express()
+
+app.listen(8080);
+
+
+/******************************************************************************
+Account APIs
+*******************************************************************************/
+
+app.post('/api/login', function(req, res, next) {
+
+});
+
+app.post('/api/createaccount', function(req, res, next) {
+
+});
+
+app.post('/api/editaccount', function(req, res, next) {
+
+});
+
+app.post('/api/deleteaccount', function(req, res, next) {
+
+});
+
+app.post('/api/getuser', function(req, res, next) {
+
+});
+
+/******************************************************************************
+News APIs
+*******************************************************************************/
+
+
+/******************************************************************************
+Webinars APIs
+*******************************************************************************/
+
+
+/******************************************************************************
+Project APIs
+*******************************************************************************/
+
+app.post('/api/createproject', function(req, res, next) {
+
+});
+
+app.post('/api/editproject', function(req, res, next) {
+
+});
+
+app.post('/api/deleteproject', function(req, res, next) {
+
+});
+
+/******************************************************************************
+Search APIs
+*******************************************************************************/
+
+app.post('/api/searchusers', function(req, res, next) {
+
+});
+
+app.post('/api/searchprojects', function(req, res, next) {
+
+});

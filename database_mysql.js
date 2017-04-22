85c0bfd790034f677eb17eeb2ac256a95ae9c2db
Joe Salter <jcsalter@princeton.edu>
2017-04-22 16:08:31 -0400

    mariadb removed from dependencies

--- database_mysql.js
+++ database_mysql.js
@@ -1,20 +1,20 @@
 let mysql = require('mysql')
 var Database = [];
 
-var mysqlConnection = mysql.createConnection({
-	host     : 'mariadb-152.wc2',
-	user     : '543772_stg_e4c',
-	password : '5mPEyk3HDSs7',
-	database : '543772_stg_e4c',
-});
+// var mysqlConnection = mysql.createConnection({
+// 	host     : 'mariadb-152.wc2.',
+// 	user     : '543772_stg_e4c',
+// 	password : '5mPEyk3HDSs7',
+// 	database : '543772_stg_e4c',
+// });
 
-mysqlConnection.connect(function(err) {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
+// mysqlConnection.connect(function(err) {
+// 	if (err) {
+// 		console.error('error connecting: ' + err.stack);
+// 		return;
+// 	}
 
-	console.log('connected as id ' + mysqlConnection.threadId);
-});
+// 	console.log('connected as id ' + mysqlConnection.threadId);
+// });
 
 module.exports = Database;

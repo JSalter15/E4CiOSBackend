59d13954c375e2daca06ba2096d2c390c81a4557
Joe Salter <jcsalter@princeton.edu>
2017-05-04 23:24:47 -0400

    async removed

--- database_e4c.js
+++ database_e4c.js
@@ -1,6 +1,5 @@
 let pg = require('pg');
 var Database = [];
-let async = require('async');
 
 const PG_DATABASE_URL = 'postgres://xeqtnkjlbzfhad:0f8eb3c9a777c80ad960ea9ac1dd010596b1991daf523f65db58682308d1fab7@ec2-23-23-223-2.compute-1.amazonaws.com:5432/d7o63skrv6brj5'
 pg.defaults.ssl = true;
